40ce5aa0005fe4ce1d8a2755478f22ab5a9530a1
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-26 08:39:15 -0300

    feat: :sparkles: add modal

---
+++ main.js
@@ -0,0 +1,33 @@
+const newScheduleButtons = document.querySelectorAll(".new-schedule");
+const modal = document.getElementById("modal");
+const container = document.querySelector(".container");
+const newScheduleButtonContainer = document.querySelector(
+  "#new-schedule-container"
+);
+
+newScheduleButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    modal.classList.remove("hiding");
+    container.classList.add("blur");
+    hideButton(button);
+  })
+);
+
+const closeModal = document.querySelector("#close-modal");
+closeModal.onclick = function () {
+  modal.classList.add("hiding");
+  container.classList.remove("blur");
+  showButtons();
+};
+
+function hideButton(button) {
+  button.classList.add("hiding");
+  newScheduleButtonContainer.classList.add("hiding");
+}
+
+function showButtons() {
+  newScheduleButtons.forEach((button) => {
+    button.classList.remove("hiding");
+    newScheduleButtonContainer.classList.remove("hiding");
+  });
+}
