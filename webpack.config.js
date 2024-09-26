c59786f9b38634614c06694b6c412b3bec2a157c
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-26 10:26:10 -0300

    chore: :wrench: add favicon config to HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve("src", "assets", "logo.svg"),
     }),
   ],
 };
